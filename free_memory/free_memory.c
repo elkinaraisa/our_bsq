082e10819aba2b59d5abfb3a6df367e4ecf05578
elkinaraisa <71191499+elkinaraisa@users.noreply.github.com>
2020-09-14 17:46:00 +0300

    no need to use clean of char matrix
    only clean of int currently

--- free_memory/free_memory.c
+++ free_memory/free_memory.c
@@ -1,6 +1,6 @@
 #include "bsq_header.h"
 
-void            clean_int_matrix(int **int_matrix, int row)
+void            clean_matrix(int **int_matrix, int row)
 {
     int         i;
 
@@ -11,18 +11,4 @@ void            clean_int_matrix(int **int_matrix, int row)
         i++;
     }
     free(int_matrix);
-}
-
-
-void			clean_matrix(char **char_matrix, int row)
-{
-	int 		i;
-
-	i = 0;
-    while (i < row)
-	{
-		free(char_matrix[i]);
-		i++;
-	}
-	free(char_matrix);
 }
